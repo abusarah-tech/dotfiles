b101410aca21f80d019282ae7e5c6f77fe90e1f7
Mohamed Houssein Douici <douici1@gmail.com>
2024-10-03 21:20:56 +0200

    chore(neovim): add the support for graphql and set up the clipboard

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,29 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.filetype.add({
+  extension = {
+    graphql = "graphql",
+    gql = "graphql",
+    typescript = "graphql",
+    typescriptreact = "graphql",
+  },
+})
+
+vim.g.clipboard = {
+  name = "xclip-wsl",
+  copy = {
+    ["+"] = "xclip -selection clipboard",
+    ["*"] = "xclip -selection primary",
+  },
+  paste = {
+    ["+"] = "xclip -selection clipboard -o",
+    ["*"] = "xclip -selection primary -o",
+  },
+  cache_enabled = 0,
+}
+
+vim.cmd([[set clipboard+=unnamedplus]])
+
+vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-c>", '"+yy', { noremap = true, silent = true })
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,19 @@
 return {
   {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    config = true,
+  },
+  {
+    "ellisonleao/dotenv.nvim",
+    config = function()
+      require("dotenv").setup({
+        enable_on_load = true,
+        verbose = false,
+      })
+    end,
+  },
+  {
     "folke/edgy.nvim",
     event = "VeryLazy",
     opts = {},
@@ -15,5 +29,48 @@ return {
     dependencies = {
       "tpope/vim-fugitive",
     },
+  },
+  {
+    "jparise/vim-graphql",
+    ft = { "graphql", "typescript", "typescriptreact" },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        graphql = {
+          filetypes = { "graphql", "typescript", "typescriptreact" },
+          root_dir = function(fname)
+            return require("lspconfig").util.root_pattern(
+              ".graphqlrc.yaml",
+              ".graphqlrc.yml",
+              ".graphqlrc",
+              ".graphql.config.js",
+              ".graphql.config.ts",
+              ".graphqlrc.js",
+              ".graphqlrc.ts"
+            )(fname) or require("lspconfig").util.find_git_ancestor(fname)
+          end,
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "graphql", "typescript" })
+      end
+      opts.highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { "graphql" },
+      }
+      opts.indent = { enable = true }
+      opts.query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      }
+    end,
   },
 }
