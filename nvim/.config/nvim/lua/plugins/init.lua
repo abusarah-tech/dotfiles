9af3af1b726411e0917ea339bb222bc1b92680cb
Mohamed Houssein Douici <douici1@gmail.com>
2024-10-26 04:01:47 +0200

    chore: change zellij theme and neovim colorscheme

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,14 +1,25 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+  { "techtuner/aura-neovim" },
+  { "ribru17/bamboo.nvim" },
+  {
+    "sainnhe/sonokai",
+    config = function()
+      vim.g.sonokai_style = "default"
+      vim.g.sonokai_enable_italic = true
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "sonokai",
     },
   },
+
+  { "imsnif/kdl.vim" },
 
   -- change trouble config
   {
